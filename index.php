ca4d965b4e08c9d0c80bf355af8687e345135c8b
unknown <goldarmony8776@gmail.com>
2025-06-12 10:13:40 -0500

    index y css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="UTF-8" />
     <title>MOTOS</title>
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
   </head>
   <body>
     <header class="header">
