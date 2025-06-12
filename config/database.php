4142dadc83cf368631301505bbaf3bd2f88e2a1b
danieltaborda636 <ctabordagarzon@gmail.com>
2025-06-12 11:45:39 -0500

    coneccion a la base de datos

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+/*guardar la configuracion de la base de datos     */
+define('DB_SERVIDOR','localhost');
+define('DB_USUARIO','root');
+define('DB_CONTRASENA','');
+define('DB_NOMBRE','tienda_sena');
+
+//crear la conexion a la DB
+$conexion = mysqli_connect(DB_SERVIDOR, DB_USUARIO, DB_CONTRASENA , DB_NOMBRE);   // ESTE ES EL ORDDEN QUE DEBE DE LLEVAR
+
+//revisar la conexion
+if($conexion === false){
+    die("Error: no se puede conectar" . mysqli_connect_error());
+
+}
+?>
