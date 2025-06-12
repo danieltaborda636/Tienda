95ca5eaa6d1cfebbb8270f4b947b4375809e28ce
Valentina <vr9446691@gmail.com>
2025-06-12 11:41:30 -0500

    creacion de los includes

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+        <!-- PIE DE PÁGINA -->
+        <footer id="pie">
+            <p>Desarrollado por Valentina, Jacob y Calos (VCJ) &copy; 2025</p>
+        </footer>
+        
+    </body>
+</html>
