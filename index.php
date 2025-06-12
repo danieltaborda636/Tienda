42172486fce1be8d1f10a51c3e86b9457bb82cd8
unknown <goldarmony8776@gmail.com>
2025-06-12 10:12:54 -0500

    index y css

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+?>
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <title>MOTOS</title>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <header class="header">
+      <div class="container">
+        <div class="btn-menu">
+          <label for="btn-menu">☰</label>
+        </div>
+        <div class="logo">
+          <!-- <img src="img/vcj.jpeg" alt=""> -->
+          <h1>VCJ MOTOS</h1>
+        </div>
+        <!-- <nav class="menu">
+          <a href="#">Urbanas</a>
+          <a href="#">Todo Terreno</a>
+          <a href="#">Deportivas</a>
+          <a href="#">Automaticas</a>
+          <a href="#">SemiAutomaticas</a>
+          <a href="#">Adventure</a>
+          <a href="#">Enduro</a>
+          <a href="#">Super Deportivas</a>
+        </nav> -->
+      </div>
+    </header>
+    <div class="capa"></div>
+    <input type="checkbox" id="btn-menu" />
+    <div class="container-menu">
+      <div class="cont-menu">
+        <nav>
+          <h1>Inicia Sesión</h1>
+          <form action="">
+            <p>Email:</p>
+            <input type="text" placeholder="Email">
+            <p>Contraseña:</p>
+            <input type="text" placeholder="Contraseña">
+            <button class="btn-iniciar">Iniciar Sesión</button>
+          </form>
+        </nav>
+        <label for="btn-menu">✖️</label>
+      </div>
+    </div>
+  </body>
+</html>
