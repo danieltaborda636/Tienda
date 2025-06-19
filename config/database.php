596488b8c0aed32976c3a4c338d844a1d51f11b0
Valentina <vr9446691@gmail.com>
2025-06-19 08:35:15 -0500

    Creacion del producto

--- config/database.php
+++ config/database.php
@@ -2,10 +2,13 @@
 /*guardar la configuracion de la base de datos     */
 class Database{
     public static function connect(){
-        define('DB_SERVIDOR','localhost');
-        define('DB_USUARIO','root');
-        define('DB_CONTRASENA','');
-        define('DB_NOMBRE','tienda_sena');
+        if (!defined('DB_SERVIDOR')) {
+            define('DB_SERVIDOR','localhost');
+            define('DB_USUARIO','root');
+            define('DB_CONTRASENA','');
+            define('DB_NOMBRE','tienda_sena');
+}
+
 
         //crear la conexion a la DB
         $conexion = mysqli_connect(DB_SERVIDOR, DB_USUARIO, DB_CONTRASENA , DB_NOMBRE);   // ESTE ES EL ORDDEN QUE DEBE DE LLEVAR
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'config/database.php';
 include 'views/layouts/header.php';
 ?>
@@ -12,9 +13,41 @@ include 'views/layouts/header.php';
         if (isset($_GET['registro'])) {
             include './views/user/register.php';
         }
-        ?>  
+        ?>
+          
     </ul>
 
+<?php
+// Miramos si en la URL hay algo como ?controller=algo&action=otraCosa
+if (isset($_GET['controller']) && isset($_GET['action'])) {
+    // Guardamos el nombre del controlador y lo que queremos que haga (la acción)
+    $nombreControlador = $_GET['controller'];
+    $accion = $_GET['action'];
+
+    // Buscamos el archivo del controlador según lo que vino en la URL
+    $archivo = 'controllers/controlador-' . $nombreControlador . '.php';
+
+    // Le ponemos Controlador al principio para formar el nombre de la clase
+    $clase = 'Controlador' . ucfirst($nombreControlador); 
+
+    // Si ese archivo sí existe en la carpeta, lo usamos
+    if (file_exists($archivo)) {
+        require_once $archivo;
+
+        // Creamos un nuevo objeto del controlador (como prenderlo pa que funcione)
+        $controlador = new $clase();
+
+        // Revisamos si ese controlador tiene la funcion que pedimos (como create o store)
+        if (method_exists($controlador, $accion)) {
+            $controlador->$accion(); // Aquí ya hacemos que se ejecute esa funcion, como guardar o mostrar
+        } else {
+            echo "<p> Esa función '$accion' no existe en ese controlador.</p>";
+        }
+    } else {
+        echo "<p> No encontramos ese controlador llamado '$archivo'.</p>";
+    }
+}
+?>
 
 
 
