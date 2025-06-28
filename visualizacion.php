dc390dd14dace14582f8240e729f82909db882aa
Jeycob-ui <goldarmony8776@gmail.com>
2025-06-27 22:59:45 -0500

    modificacion de visualizacion.php

--- visualizacion.php
+++ visualizacion.php
@@ -132,7 +132,7 @@ $resultado_categorias = mysqli_query($conexion, $sql_categorias);
 </div>
 
 <div class="crearproducto">
-        <label for="oferta">¿Está en oferta?</label><br>
+        <label for="oferta">¿Está disponible?</label><br>
         <select id="oferta" name="oferta">
             <option value="SI">Sí</option>
             <option value="NO">No</option>
