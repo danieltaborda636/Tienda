bf029ab30c024f538e95a512d864cafba2604db5
Jeycob-ui <goldarmony8776@gmail.com>
2025-06-28 19:05:25 -0500

    mas estilos de las cards y estilos de los detalles del producto

--- index.php
+++ index.php
@@ -40,11 +40,11 @@ include 'config/parametros.php';
 
         <?php elseif ($_SESSION['usuario']['rol'] === 'admin'): ?>
             <li><p class="text">Bienvenido, <a class="tex"><?= $_SESSION['usuario']['nombre']; ?></a></p></li>
-            <li><a class="text" href="./visualizacion.php">Ver carrito</a></li>
-            <li><a class="text" href="#">Mis pedidos</a></li>
-            <li><a class="text" href="#">Gestionar Pedidos</a></li>
-            <li><a class="text" href="./visualizacion.php">Crear producto</a></li>
-            <li><a class="text" href="?view=categoria">Crear categoría</a></li>
+            <li><a class="text" href="./visualizacion.php">Ver carrito</a></li><br>
+            <li><a class="text" href="#">Mis pedidos</a></li><br>
+            <li><a class="text" href="#">Gestionar Pedidos</a></li><br>
+            <li><a class="text" href="./visualizacion.php">Crear producto</a></li><br>
+            <li><a class="text" href="?view=categoria">Crear categoría</a></li><br>
             <li><a class="text" href="controllers/logout.php">Cerrar sesión</a></li>
 
         <?php else: ?>
