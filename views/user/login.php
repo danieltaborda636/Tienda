d1d8ae5e7f2e66a7ec38f55a8f12c9e5faa77e7e
Jeycob-ui <goldarmony8776@gmail.com>
2025-07-02 22:20:48 -0500

    estilos

--- views/user/login.php
+++ views/user/login.php
@@ -5,32 +5,29 @@ include '../../config/parametros.php';
 ?>
 <title>Iniciar Sesión</title>
 <link rel="stylesheet" href="<?= base_url ?>/assets/css/login.css">
-<div class="Todo">
+<!-- <div class="Todo"> -->
 <div class="containerLogin">
+    <img src="../../assets/img/logo_moto.jpeg" alt="">
     <h1>Iniciar sesión</h1>
     <?php if (isset($_GET['login'])): ?>
         <div class="mensaje">
             <?php if ($_GET['login'] == 'error'): ?>
                 <p class="mensaje-error">Email o contraseña incorrectos.</p>
+                <?php endif; ?>
+            </div>
             <?php endif; ?>
-        </div>
-    <?php endif; ?>
-    <form action="<?= base_url ?>/controllers/UserController.php" method="POST">
+            <form action="<?= base_url ?>/controllers/UserController.php" method="POST">
         <p class="parrafos">Correo electrónico:</p>
         <input type="email" name="email" placeholder="Gmail" required>
 
         <p class="parrafos">Contraseña:</p>
-        <input type="password" name="password" placeholder="Contraseña" required>
+        <input type="password" name="password" placeholder="Contraseña" required><br><br>
 
-        <button class="btn-iniciar" type="submit" name="login">Iniciar sesión</button><br>
+        <button class="btn-iniciar" type="submit" name="login">Iniciar sesión</button><br><br>
         <button class="btn-atras" onclick="location.href='<?=base_url?>/index.php'">Atrás</button>
+        <!-- <img src="../../assets/img/logo_moto.jpeg" alt=""> -->
     </form>
 </div>
-<!-- <div class="imagen"> -->
-    <img src="../../assets/img/logo_moto.jpeg" alt="">
-<!-- </div> -->
-</div>
-
 
 <?php 
 include '../layouts/footer.php'
