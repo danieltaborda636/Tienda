9eaacfdb27998aad9f99e5c432f8c2c6e5cf021f
Jeycob-ui <goldarmony8776@gmail.com>
2025-07-06 15:42:27 -0500

    estilos

--- views/user/categoria.php
+++ views/user/categoria.php
@@ -4,13 +4,13 @@ include '../layouts/header.php';
 include '../../config/parametros.php';
 ?>
 <title>Crear categoria</title>
-<link rel="stylesheet" href="<?= base_url ?>/assets/css/categorias.css">
+<link rel="stylesheet" href="<?= base_url ?>/assets/css/categoria.css">
 
 <div class="contenedor">
     <h1>Nueva Categoria</h1>
     <form action="<?= base_url ?>/controllers/categoryController.php" method="POST">
-        <p class="parrafos">Nombre de la nueva categoria :</p>
-        <input type="text" name="categoria" placeholder="Nombre de categoria" required>
+        <p class="parrafos">Nombre de la nueva categoria :</p><br>
+        <input type="text" name="categoria" placeholder="Nombre de categoria" required><br><br>
 
         <button class="btn-iniciar" type="submit">Crear Categoria</button>
     </form>
