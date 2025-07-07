0ee9666d325cb6e7ac4ff3bef09f329593ff5246
Jeycob-ui <goldarmony8776@gmail.com>
2025-07-07 17:30:18 -0500

    ...

--- index.php
+++ index.php
@@ -15,10 +15,10 @@ include 'config/parametros.php';
             <h1><li><p class="text">Bienvenido, <a class="tex"><?= htmlspecialchars($_SESSION['usuario']['nombre']); ?></a></p></li></h1>
             <li><a class="text" href="./carrito.php">Ver carrito</a></li><br>
             <li><a class="text" href="#">Mis pedidos</a></li><br>
-            <li><a class="text" href="./productos.php">Gestionar producto</a></li><br>
+            <li><a class="text" href="./productos.php">Gestionar productos</a></li><br>
             <li><a class="text" href="./crear_producto.php">Crear producto</a></li><br>
             <li><a class="text" href="views/user/categoria.php">Crear categoría</a></li><br>
-            <li><a class="text" href="./categorias.php">Gestionar categoría</a></li><br>
+            <li><a class="text" href="./categorias.php">Gestionar categorías</a></li><br>
             <li><a class="text" href="controllers/logout.php">Cerrar sesión</a></li>
         <?php else: ?>
             <li><p class="text">Bienvenido, <a class="tex"><?= htmlspecialchars($_SESSION['usuario']['nombre']); ?></a></p></li>
